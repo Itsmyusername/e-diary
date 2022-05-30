9b0fd16061e62817228a859605a06850e80732b8
Itsmyusername <25662642+Itsmyusername@users.noreply.github.com>
2022-05-30 21:12:58 +0300

    add try/except

--- scripts/scripts.py
+++ scripts/scripts.py
@@ -1,6 +1,7 @@
 import random
-from datacenter.models import Schoolkid, Subject, Lesson, Mark, Commendation,
-Сhastisement
+from django.core.exceptions import ObjectDoesNotExist
+from datacenter.models import (Schoolkid, Subject, Lesson, Mark, Commendation,
+Сhastisement)
 
 
 COMMENDATIONS = [
@@ -12,7 +13,10 @@ COMMENDATIONS = [
 
 
 def fix_marks(schoolkid_name):
-    child = Schoolkid.objects.get(full_name__contains=schoolkid_name)
+    try:
+        child = Schoolkid.objects.get(full_name__contains=schoolkid_name)
+    except ObjectDoesNotExist:
+        print('Does Not Exist!')
     marks = Mark.objects.filter(schoolkid=child, points__in=[2, 3])
     for mark in marks:
         mark.points = random.choice([4, 5])
@@ -20,15 +24,24 @@ def fix_marks(schoolkid_name):
 
 
 def remove_chastisements(schoolkid_name):
-    child = Schoolkid.objects.get(full_name__contains=schoolkid_name)
+    try:
+        child = Schoolkid.objects.get(full_name__contains=schoolkid_name)
+    except ObjectDoesNotExist:
+        print('Does Not Exist!')
     chastisements = Сhastisement.objects.filter(schoolkid=child)
     chastisements.delete()
 
 
 def create_commendation(schoolkid_name, subject_name):
-    child = Schoolkid.objects.get(full_name__contains=schoolkid_name)
-    subject = Subject.objects.get(title__contains=subject_name,
+    try:
+        child = Schoolkid.objects.get(full_name__contains=schoolkid_name)
+    except ObjectDoesNotExist:
+        print('Does Not Exist!')
+    try:
+        subject = Subject.objects.get(title__contains=subject_name,
                                   year_of_study=child.year_of_study)
+    except ObjectDoesNotExist:
+        print('Does Not Exist!')
     lesson = Lesson.objects.filter(group_letter=child.group_letter,
                                    subject=subject).order_by('?').first()
     teacher = lesson.teacher
