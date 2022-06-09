4a7965b4bd9cfa96cf0aaaf0c4bc25b1e84be738
Itsmyusername <25662642+Itsmyusername@users.noreply.github.com>
2022-06-09 11:42:09 +0300

    Update scripts.py

--- scripts/scripts.py
+++ scripts/scripts.py
@@ -31,8 +31,8 @@ def get_child(schoolkid_name):
 
 
 def fix_marks(schoolkid_name):
-    func = get_child(schoolkid_name)
-    marks = Mark.objects.filter(schoolkid=func, points__in=[2, 3])
+    child = get_child(schoolkid_name)
+    marks = Mark.objects.filter(schoolkid=child, points__in=[2, 3])
     for mark in marks:
         mark.points = random.choice([4, 5])
         mark.save()
